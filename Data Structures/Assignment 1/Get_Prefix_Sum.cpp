4d0b35f90037fc0d2628ec6f6339217725987482
saba rahman <sabarahman.official@gmail.com>
2024-06-13 20:44:24 +0600

    problem 1 solved

--- Data Structures/Assignment 1/Get_Prefix_Sum.cpp
+++ Data Structures/Assignment 1/Get_Prefix_Sum.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin>>n;
+    long long int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    long long int prefarr[n];
+    prefarr[0]=arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        prefarr[i]=prefarr[i-1]+arr[i];
+    }
+    for (int i = n-1; i >=0; i--)
+    {
+        cout<<prefarr[i]<<" ";
+    }
+    
+    
+    
+    
 
 
 
