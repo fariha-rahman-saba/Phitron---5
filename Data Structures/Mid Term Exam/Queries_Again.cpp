91b9033fde667cd0530f048646afdf301d7bed4c
saba rahman <sabarahman.official@gmail.com>
2024-07-06 14:44:59 +0600

    problem 4 solved

--- Data Structures/Mid Term Exam/Queries_Again.cpp
+++ Data Structures/Mid Term Exam/Queries_Again.cpp
@@ -1,112 +1,94 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
-{
+class Node {
 public:
     int val;
     Node *prev;
     Node *next;
 
-    Node(int val)
-    {
+    Node(int val) {
         this->val = val;
         this->prev = NULL;
         this->next = NULL;
     }
 };
 
-bool insert_at(Node *&head, Node *&tail, int pos, int val,int size)
-{
+bool insert_at(Node *&head, Node *&tail, int pos, int val, int size) {
     Node *newNode = new Node(val);
     Node *tmp = head;
 
-    if (pos < 0 || pos > size)
-    {
+    if (pos < 0 || pos > size) {
         return false;
     }
-    else if (pos == 0)
-    {
-        if (head == NULL)
-        {
+    else if (pos == 0) {
+        if (head == NULL) {
             head = newNode;
             tail = newNode;
-        }
-        else
-        {
+        } else {
             newNode->next = head;
             head->prev = newNode;
             head = newNode;
         }
     }
-
-    else if (pos == size)
-    {
-        tail->next = newNode;
-        newNode->prev = tail;
-        tail = newNode;
+    else if (pos == size) {
+        if (tail == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        }
     }
-    else
-    {
-        for (int i = 0; i <= pos - 1; i++)
-        {
+    else {
+        for (int i = 0; i < pos - 1; i++) {
             tmp = tmp->next;
         }
         newNode->next = tmp->next;
+        newNode->prev = tmp;
         tmp->next->prev = newNode;
         tmp->next = newNode;
-        newNode->prev = tmp;
     }
     return true;
 }
 
-void print_from_left(Node *head)
-{
+void print_from_left(Node *head) {
     cout << "L -> ";
     Node *tmp = head;
-    while (tmp != NULL)
-    {
+    while (tmp != NULL) {
         cout << tmp->val << " ";
         tmp = tmp->next;
     }
     cout << endl;
 }
 
-void print_from_right(Node *tail)
-{
+void print_from_right(Node *tail) {
     cout << "R -> ";
     Node *tmp = tail;
-    while (tmp != NULL)
-    {
+    while (tmp != NULL) {
         cout << tmp->val << " ";
         tmp = tmp->prev;
     }
     cout << endl;
 }
 
-int main()
-{
-
+int main() {
     int t, x, v;
     cin >> t;
-    int size=0;
+    int size = 0;
 
     Node *head = NULL;
     Node *tail = NULL;
 
-    while (t--)
-    {
+    while (t--) {
         cin >> x >> v;
-        int res = insert_at(head, tail, x, v,size);
-        if (res==1)
-        {
+        bool res = insert_at(head, tail, x, v, size);
+        if (res) {
             size++;
             print_from_left(head);
             print_from_right(tail);
-        }
-
-        else
-        {
+        } else {
             cout << "Invalid\n";
         }
     }
