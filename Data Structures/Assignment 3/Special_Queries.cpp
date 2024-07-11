049d611f5b3b330dee4a2d3771d35579db11a7bb
saba rahman <sabarahman.official@gmail.com>
2024-07-11 22:33:35 +0600

    problem 3 solved

---
+++ Data Structures/Assignment 3/Special_Queries.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t, q;
+    string s;
+    queue<string> que;
+
+    cin >> t;
+    while (t--)
+    {
+        cin >> q;
+        if (q == 0)
+        {
+            cin >> s;
+            que.push(s);
+        }
+        else
+        {
+            if (que.empty())
+            {
+                cout << "Invalid" << endl;
+            }
+
+            else
+            {
+                cout << que.front() << endl;
+                que.pop();
+            }
+        }
+    }
+
+    return 0;
+}
