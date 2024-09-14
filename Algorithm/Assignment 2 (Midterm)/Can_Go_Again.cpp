53a633880511cd8efc98d09752b9227545616b9a
saba rahman <sabarahman.official@gmail.com>
2024-09-14 13:24:53 +0600

    problem 3 solved

--- Algorithm/Assignment 2 (Midterm)/Can_Go_Again.cpp
+++ Algorithm/Assignment 2 (Midterm)/Can_Go_Again.cpp
@@ -1,13 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int INF = 1e9+5;
+const long long INF = 1e18 + 5;
 struct Edge {
-    int u, v, w;
+    int u, v;
+    long long w;
 };
 
 vector<Edge> edges;
-vector<int> dist;
+vector<long long> dist;
 int n, m;
 
 bool bellmanFord(int src) {
@@ -15,14 +16,14 @@ bool bellmanFord(int src) {
     dist[src] = 0;
 
     for (int i = 1; i <= n - 1; ++i) {
-        for (auto &edge : edges) {
+        for (const auto& edge : edges) {
             if (dist[edge.u] < INF && dist[edge.u] + edge.w < dist[edge.v]) {
                 dist[edge.v] = dist[edge.u] + edge.w;
             }
         }
     }
 
-    for (auto &edge : edges) {
+    for (const auto& edge : edges) {
         if (dist[edge.u] < INF && dist[edge.u] + edge.w < dist[edge.v]) {
             return false;
         }
@@ -34,14 +35,12 @@ bool bellmanFord(int src) {
 int main() {
     cin >> n >> m;
 
-    if (n == 0 || m == 0) {
-        cout << "Not Possible" << endl;
-        return 0;
-    }
-
     edges.resize(m);
     for (int i = 0; i < m; ++i) {
-        cin >> edges[i].u >> edges[i].v >> edges[i].w;
+        int u, v;
+        long long w;
+        cin >> u >> v >> w;
+        edges[i] = {u, v, w};
     }
 
     int source;
@@ -59,9 +58,7 @@ int main() {
         int dest;
         cin >> dest;
 
-        if (source == dest) {
-            cout << 0 << endl;
-        } else if (dist[dest] == INF) {
+        if (dist[dest] == INF) {
             cout << "Not Possible" << endl;
         } else {
             cout << dist[dest] << endl;
