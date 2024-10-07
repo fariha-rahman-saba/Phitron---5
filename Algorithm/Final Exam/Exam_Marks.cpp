61eb0de393b7b2dd9cc5569b2fe8db15e59aee52
saba rahman <sabarahman.official@gmail.com>
2024-10-07 17:53:06 +0600

    problem 4 solved

--- Algorithm/Final Exam/Exam_Marks.cpp
+++ Algorithm/Final Exam/Exam_Marks.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool canAchievePerfectScore(int n, int target, vector<int>& marks) {
+bool canAchievePerfectScore(int n, vector<int>& marks, int m) {
+    int target = 1000 - m;
+    if (target < 0) return false;
+
     vector<bool> dp(target + 1, false);
     dp[0] = true;
 
@@ -19,13 +22,14 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, target;
-        cin >> n >> target;
+        int n, m;
+        cin >> n >> m;
         vector<int> marks(n);
         for (int i = 0; i < n; i++) {
             cin >> marks[i];
         }
-        if (canAchievePerfectScore(n, target, marks)) {
+
+        if (canAchievePerfectScore(n, marks, m)) {
             cout << "YES" << endl;
         } else {
             cout << "NO" << endl;
