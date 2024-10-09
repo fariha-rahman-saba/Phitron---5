f8b0d1e60ecfbbed835ea9963bf2b50edae9a0db
saba rahman <sabarahman.official@gmail.com>
2024-10-09 08:15:50 +0600

    problem 5 solved

--- Algorithm/Final Exam/water_final.cpp
+++ Algorithm/Final Exam/water_final.cpp
@@ -11,10 +11,10 @@ int main()
         int n;
         cin >> n;
         int heights[n];
-        int first_max = -1;
-        int second_max = -1;
-        int first_idx;
-        int second_idx;
+        int first_max = 0;
+        int second_max = 0;
+        int first_idx = 0;
+        int second_idx = 0;
 
         for (int i = 0; i < n; i++)
         {
@@ -31,13 +31,12 @@ int main()
         }
         for (int i = 0; i < n; i++)
         {
-            if (heights[i] > second_max && heights[i] < first_max)
+            if (heights[i] > second_max && heights[i] <= first_max && i!=first_idx)
             {
                 second_max = heights[i];
                 second_idx = i;
             }
         }
-        
         if(first_idx>second_idx) cout<< second_idx<<" "<<first_idx<<endl;
         else cout << first_idx << " " << second_idx << endl;
     }
